317308ac7f5fa33c8838c485eaaf851d7824ae26
Kevin-Ax <kevin.lourencini@ufv.br>
2021-05-04 19:04:26 -0300

    Addition of reversecard

---
+++ reversecard.h
@@ -0,0 +1,16 @@
+#ifndef REVERSECARD_H_
+#define REVERSECARD_H_
+
+#include "coloredCard.h"
+#include <string>
+#include "game.h"
+
+class ReverseCard :: public ColoredCard{
+	public:
+		ReverseCard();
+		ReverseCard* copy() override;
+		void reverse_orientation(Game game);
+		int get_number() override;
+};
+
+#endif
