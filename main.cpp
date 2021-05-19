15b25e670dda20b13d5d135e9c6dc001381b28dc
Guilherme-jose <guijose.bitencourt1999@gmail.com>
2021-05-19 18:20:38 -0300

    add colored blackCard sprites

--- main.cpp
+++ main.cpp
@@ -32,19 +32,6 @@ ALLEGRO_EVENT_QUEUE* queue;
 ALLEGRO_DISPLAY* disp;
 ALLEGRO_FONT* font;
 
-void loadSprites() {
-  //for (int i = 0; i < game.get_board()->get_deck()->get_cards().size(); i++) {
-  //  game.get_board()->get_deck()->get_cards().at(i)->set_sprite(al_load_bitmap(game.get_board()->get_deck()->get_cards().at(i)->get_spriteName().c_str()));
-  //}
-  for (int j = 0; j < game.get_players(); j++) {
-    for(int i = 0; i < game.get_playerList().at(j)->get_hand()->get_cards().size(); i++) {
-      game.get_playerList().at(j)->get_hand()->get_cards().at(i)->set_sprite(al_load_bitmap(game.get_playerList().at(j)->get_hand()->get_cards().at(i)->get_spriteName().c_str()));
-    }
-  }
-
-  game.get_board()->get_stack()->get_cards().back()->set_sprite(al_load_bitmap(game.get_board()->get_stack()->get_cards().back()->get_spriteName().c_str()));
-}
-
 int ClickedCard() {
   ALLEGRO_MOUSE_STATE state;
   al_get_mouse_state(&state);
--- util.h
+++ util.h
@@ -16,5 +16,6 @@
 #include "card.h"
 
 Player* activePlayer();
+void loadSprites();
 
 #endif
